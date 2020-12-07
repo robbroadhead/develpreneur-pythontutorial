1e0151ae0338091befb194625801cec9a72e4ded
EC2 Default User <ec2-user@ip-172-16-1-145.ec2.internal>
2020-12-07 14:07:09 +0000

    Day 15 Changes

--- dpproject/models.py
+++ dpproject/models.py
@@ -10,13 +10,14 @@ class lkpStatus(models.Model):
         return self.name
     
 class Task(models.Model):
-    name = models.CharField(max_length=50,blank=True)
-    duedate = models.DateTimeField(null=True,blank=True)
-    completiondate = models.DateTimeField(null=True,blank=True)
-    status = models.ForeignKey(lkpStatus, on_delete=models.CASCADE)
-    minutes_spent = models.IntegerField(null=True,blank=True)
-    creationdate = models.DateTimeField(default=datetime.datetime.now)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50,blank=True,verbose_name="Name",help_text="Name of the task")
+    duedate = models.DateTimeField(null=True,blank=True,verbose_name="Date Due")
+    completiondate = models.DateTimeField(null=True,blank=True,verbose_name="Completed Date")
+    status = models.ForeignKey(lkpStatus, on_delete=models.CASCADE,verbose_name="Status",db_index=True)
+    attachment = models.FileField(null=True,blank=True)
+    minutes_spent = models.IntegerField(null=True,blank=True,verbose_name="Time Spent",help_text="Minutes spent working this task",default=0)
+    creationdate = models.DateTimeField(default=datetime.datetime.now,verbose_name="Created")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name="Owner")
 
     def __str__(self):
         return self.name + "(" +  self.status.shortname + ")"
