bba15a124c3c8c7a2caa303cd2310897ae04ba36
EC2 Default User <ec2-user@ip-172-16-1-145.ec2.internal>
2021-01-02 14:49:06 +0000

    Day 20 changes. Added api script

---
+++ dpproject/api.py
@@ -0,0 +1,38 @@
+#! /home/linuxbrew/.linuxbrew/bin/python3
+
+from flask import Flask, g, make_response, request, json, jsonify
+import mysql
+from mysql.connector import Error
+
+app = Flask(__name__)
+
+config = {
+    'user' : 'root',
+    'password': 'develpreneur',
+    'host': '127.0.0.1',
+    'database': 'dpproject',
+    'raise_on_warnings': True
+}
+db = mysql.connector.connect(**config)
+cursor = db.cursor()
+
+@app.route("/db")
+def dbTest():
+    try:
+        query = "select * from dpproject_task"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+    except Error as e:
+        return make_response(jsonify(e),500)    
+
+    return make_response(jsonify(rows),200)
+
+@app.route("/")
+def home():
+    return "API is Active!"
+
+@app.route("/about")
+def about():
+    return "Version 1.0"
+
+app.run()
